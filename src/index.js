e22ab6455e930181cf27c57582f484bc08068799
felipeog <17603069+felipeog@users.noreply.github.com>
2023-11-24 00:24:26 -0300

    Init

---
+++ src/index.js
@@ -0,0 +1,153 @@
+import { gsap } from "gsap";
+
+const ns = "http://www.w3.org/2000/svg";
+const id = `window:${crypto.randomUUID()}`;
+const color =
+  "rgb(" +
+  Math.random() * 255 +
+  ", " +
+  Math.random() * 255 +
+  ", " +
+  Math.random() * 255 +
+  ")";
+
+addEventListener("storage", (event) => {
+  const svg = document.querySelector("svg");
+
+  if (event.oldValue && !event.newValue) {
+    const line = document.querySelector(`line[data-id="${event.key}"]`);
+    const circle = document.querySelector(`circle[data-id="${event.key}"]`);
+
+    svg.removeChild(line);
+    svg.removeChild(circle);
+  }
+
+  if (!event.oldValue && event.newValue) {
+    const w = JSON.parse(event.newValue);
+
+    const line = document.createElementNS(ns, "line");
+    line.setAttribute("data-id", w.id);
+    line.setAttribute("x1", w.position.x);
+    line.setAttribute("y1", w.position.y);
+    line.setAttribute("x2", w.position.x);
+    line.setAttribute("y2", w.position.y);
+    line.setAttribute("stroke", "black");
+    svg.appendChild(line);
+
+    const circle = document.createElementNS(ns, "circle");
+    circle.setAttribute("data-id", w.id);
+    circle.setAttribute("cx", w.position.x);
+    circle.setAttribute("cy", w.position.y);
+    circle.setAttribute("r", 50);
+    circle.setAttribute("fill", w.color);
+    svg.appendChild(circle);
+  }
+});
+
+function render() {
+  const currentWindow = JSON.parse(localStorage.getItem(id));
+  const otherWindowsKeys = Object.keys(localStorage).filter(
+    (key) => key !== currentWindow.id && key.startsWith("window:")
+  );
+  const otherWindows = otherWindowsKeys.map((key) =>
+    JSON.parse(localStorage.getItem(key))
+  );
+
+  const svg = document.querySelector("svg");
+  svg.setAttribute("viewBox", `0 0 ${window.innerWidth} ${window.innerHeight}`);
+  svg.setAttribute("width", window.innerWidth);
+  svg.setAttribute("height", window.innerHeight);
+
+  gsap.to(`[data-id="${currentWindow.id}"]`, {
+    cx: currentWindow.position.x - window.screenLeft,
+    cy: currentWindow.position.y - window.screenTop,
+  });
+
+  otherWindows.forEach((w) => {
+    gsap.to(`circle[data-id="${w.id}"]`, {
+      cx: w.position.x - window.screenLeft,
+      cy: w.position.y - window.screenTop,
+    });
+
+    gsap.to(`line[data-id="${w.id}"]`, {
+      attr: {
+        x1: w.position.x - window.screenLeft,
+        y1: w.position.y - window.screenTop,
+        x2: currentWindow.position.x - window.screenLeft,
+        y2: currentWindow.position.y - window.screenTop,
+      },
+    });
+  });
+
+  localStorage.setItem(
+    id,
+    JSON.stringify({
+      ...currentWindow,
+      position: {
+        x: window.screenLeft + window.innerWidth / 2,
+        y: window.screenTop + window.innerHeight / 2,
+      },
+    })
+  );
+
+  requestAnimationFrame(render);
+}
+
+window.onload = async () => {
+  const currentWindow = {
+    id,
+    color,
+    position: {
+      x: window.screenLeft + window.innerWidth / 2,
+      y: window.screenTop + window.innerHeight / 2,
+    },
+  };
+
+  localStorage.setItem(id, JSON.stringify(currentWindow));
+
+  const otherWindowsKeys = Object.keys(localStorage).filter(
+    (key) => key !== currentWindow.id && key.startsWith("window:")
+  );
+  const otherWindows = otherWindowsKeys.map((key) =>
+    JSON.parse(localStorage.getItem(key))
+  );
+
+  const svg = document.createElementNS(ns, "svg");
+  svg.setAttribute("viewBox", `0 0 ${window.innerWidth} ${window.innerHeight}`);
+  svg.setAttribute("width", window.innerWidth);
+  svg.setAttribute("height", window.innerHeight);
+  document.body.appendChild(svg);
+
+  otherWindows.forEach((w) => {
+    const line = document.createElementNS(ns, "line");
+    line.setAttribute("data-id", w.id);
+    line.setAttribute("x1", w.position.x);
+    line.setAttribute("y1", w.position.y);
+    line.setAttribute("x2", currentWindow.position.x);
+    line.setAttribute("y2", currentWindow.position.y);
+    line.setAttribute("stroke", "black");
+    svg.appendChild(line);
+
+    const circle = document.createElementNS(ns, "circle");
+    circle.setAttribute("data-id", w.id);
+    circle.setAttribute("cx", w.position.x);
+    circle.setAttribute("cy", w.position.y);
+    circle.setAttribute("r", 50);
+    circle.setAttribute("fill", w.color);
+    svg.appendChild(circle);
+  });
+
+  const circle = document.createElementNS(ns, "circle");
+  circle.setAttribute("data-id", currentWindow.id);
+  circle.setAttribute("cx", currentWindow.position.x);
+  circle.setAttribute("cy", currentWindow.position.y);
+  circle.setAttribute("r", 50);
+  circle.setAttribute("fill", currentWindow.color);
+  svg.appendChild(circle);
+
+  render();
+};
+
+window.onbeforeunload = () => {
+  localStorage.removeItem(id);
+};
