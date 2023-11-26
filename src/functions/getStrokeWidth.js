20ca9b4e199b7a5a064ad9017a189337aa915346
felipeog <17603069+felipeog@users.noreply.github.com>
2023-11-25 23:58:42 -0300

    Format

--- src/functions/getStrokeWidth.js
+++ src/functions/getStrokeWidth.js
@@ -3,7 +3,6 @@ export function getStrokeWidth(window1, window2) {
     window1.position.x - window2.position.x,
     window1.position.y - window2.position.y
   );
-  const strokeWidth = Math.max(1, 10 - distance / 100);
 
-  return strokeWidth;
+  return Math.max(1, 10 - distance / 100);
 }
